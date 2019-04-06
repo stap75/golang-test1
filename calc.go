d7da18ac1a5f75faae05cc275525eea552dc5b84
Stamatis Panorgios <stamatis@192.168.1.50>
2019-04-06 16:12:56 +0300

    init

---
+++ calc.go
@@ -0,0 +1,9 @@
+package calc
+
+func Add(x,y int) int {
+    return x+y
+} 
+
+func Substrack(x,y int) int {
+    return x-y
+}
---
+++ test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"calc"
+	"fmt"
+)
+
+func main() {
+
+	a := 1
+	b := 2
+	fmt.Println(a)
+	fmt.Println(calc.Add(a, b))
+}
